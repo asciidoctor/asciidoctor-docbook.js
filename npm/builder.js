fca4cdefcfd9abf84b3e9195fb020e88710debe3
Guillaume Grossetie <ggrossetie@gmail.com>
2018-04-17 16:19:15 +0200

    Upgrade to Asciidoctor.js 1.5.6

--- npm/builder.js
+++ npm/builder.js
@@ -1,14 +1,13 @@
-module.exports = Builder;
-
-var async = require('async');
-var fs = require('fs');
-var log = require('bestikk-log');
-var bfs = require('bestikk-fs');
-var download = require('bestikk-download');
-var OpalCompiler = require('bestikk-opal-compiler');
+'use strict';
+const async = require('async');
+const fs = require('fs');
+const log = require('bestikk-log');
+const bfs = require('bestikk-fs');
+const download = require('bestikk-download');
+const OpalBuilder = require('opal-compiler').Builder;
 
 function Builder () {
-  this.asciidoctorCoreVersion = '1.5.6.1';
+  this.asciidoctorCoreVersion = '1.5.6.2';
 }
 
 Builder.prototype.build = function (callback) {
@@ -22,16 +21,16 @@ Builder.prototype.build = function (callback) {
     callback();
     return;
   }
-  var builder = this;
-  var start = process.hrtime();
+  const builder = this;
+  const start = process.hrtime();
 
   async.series([
-    function (callback) { builder.clean(callback); }, // clean
-    function (callback) { builder.downloadDependencies(callback); }, // download dependencies
-    function (callback) { builder.compile(callback); }, // compile
-    function (callback) { builder.generateUMD(callback); }, // generate UMD
-    function (callback) { builder.copyToDist(callback); } // copy to dist
-  ], function () {
+    (callback) => builder.clean(callback), // clean
+    (callback) => builder.downloadDependencies(callback), // download dependencies
+    (callback) => builder.compile(callback), // compile
+    (callback) => builder.generateUMD(callback), // generate UMD
+    (callback) => builder.copyToDist(callback) // copy to dist
+  ], () => {
     log.success('Done in ' + process.hrtime(start)[0] + 's');
     typeof callback === 'function' && callback();
   });
@@ -52,45 +51,50 @@ Builder.prototype.removeBuildDirSync = function () {
 Builder.prototype.downloadDependencies = function (callback) {
   log.task('download dependencies');
 
-  var builder = this;
+  const builder = this;
   async.series([
-    function (callback) { download.getContentFromURL('https://codeload.github.com/asciidoctor/asciidoctor/tar.gz/v' + builder.asciidoctorCoreVersion, 'build/asciidoctor.tar.gz', callback); },
-    function (callback) { bfs.untar('build/asciidoctor.tar.gz', 'asciidoctor', 'build', callback); }
-  ], function () {
+    (callback) => download.getContentFromURL('https://codeload.github.com/asciidoctor/asciidoctor/tar.gz/v' + builder.asciidoctorCoreVersion, 'build/asciidoctor.tar.gz', callback),
+    (callback) => bfs.untar('build/asciidoctor.tar.gz', 'asciidoctor', 'build', callback)
+  ], () => {
     typeof callback === 'function' && callback();
   });
 };
 
 Builder.prototype.compile = function (callback) {
   log.task('compile');
-  var opalCompiler = new OpalCompiler({dynamicRequireLevel: 'ignore', defaultPaths: ['build/asciidoctor/lib']});
-  opalCompiler.compile('asciidoctor/converter/docbook5', 'build/asciidoctor-docbook5.js');
-  opalCompiler.compile('asciidoctor/converter/docbook45', 'build/asciidoctor-docbook45.js');
+  ['docbook5', 'docbook45'].forEach((module) => {
+    const opalBuilder = OpalBuilder.create();
+    opalBuilder.appendPaths('build/asciidoctor/lib');
+    opalBuilder.appendPaths('node_modules/opal-compiler/src/stdlib');
+    opalBuilder.appendPaths('lib');
+    opalBuilder.setCompilerOptions({dynamic_require_severity: 'ignore'});
+    fs.writeFileSync(`build/asciidoctor-${module}.js`, opalBuilder.build(`asciidoctor/converter/${module}`).toString(), 'utf8');
+  });
   typeof callback === 'function' && callback();
 };
 
-var templateFile = function (templateFile, context, outputFile) {
-  var template = fs.readFileSync(templateFile, 'utf8');
-  var lines = template.split('\n');
+const templateFile = function (templateFile, context, outputFile) {
+  const template = fs.readFileSync(templateFile, 'utf8');
+  const lines = template.split('\n');
   lines.forEach(function (line, index, result) {
     if (line in context) {
       result[index] = context[line];
     }
   });
-  var content = lines.join('\n');
+  const content = lines.join('\n');
   fs.writeFileSync(outputFile, content, 'utf8');
 };
 
-Builder.prototype.generateUMD = function(callback) {
+Builder.prototype.generateUMD = function (callback) {
   log.task('generate UMD');
 
   // DocBook 4.5 + DocBook 5
-  var docbookFiles = [
+  const docbookFiles = [
     'build/asciidoctor-docbook45.js',
     'build/asciidoctor-docbook5.js'
   ];
   this.concat('Asciidoctor DocBook 4.5 + 5', docbookFiles, 'build/asciidoctor-docbook.js');
-  var asciidoctorDocbookTemplateContext = {
+  const asciidoctorDocbookTemplateContext = {
     '//#{asciidoctorDocbookCode}': fs.readFileSync('build/asciidoctor-docbook.js', 'utf8')
   };
   templateFile('src/template-asciidoctor-docbook.js', asciidoctorDocbookTemplateContext, 'build/asciidoctor-docbook.js');
@@ -103,12 +107,12 @@ Builder.prototype.concat = function (message, files, destination) {
 };
 
 Builder.prototype.copyToDist = function (callback) {
-  var builder = this;
+  const builder = this;
 
   log.task('copy to dist/');
   builder.removeDistDirSync();
   bfs.copySync('build/asciidoctor-docbook.js', 'dist/main.js');
-  typeof callback === 'function' && callback(); 
+  typeof callback === 'function' && callback();
 };
 
 Builder.prototype.removeDistDirSync = function () {
@@ -116,3 +120,5 @@ Builder.prototype.removeDistDirSync = function () {
   bfs.removeSync('dist');
   bfs.mkdirsSync('dist');
 };
+
+module.exports = Builder;
