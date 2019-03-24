47ce2f95eedbaf069c7596003ab1561c0a2e34f1
Thomas Parisot <oncletom@users.noreply.github.com>
2019-03-24 18:12:34 +0100

    Make test to fail

--- test/spec.js
+++ test/spec.js
@@ -1,26 +1,15 @@
-var asciidoctor = require('asciidoctor.js')();
+var asciidoctor = require('@asciidoctor/core')();
 require('../dist/main.js')(); // Asciidoctor DocBook
 var assert = require('assert');
 
 describe('Rendering', function () {
 
-  it('should produce a docbook45 document when backend=docbook45', function () {
+  it('should not produce a docbook45 document when backend=docbook45', function () {
     var options = {attributes: ['backend=docbook45', 'doctype=book'], header_footer: true};
-    var html = asciidoctor.convert(':doctitle: DocTitle\n:docdate: 2014-01-01\n== Test', options);
-    assert.equal(html, '<?xml version="1.0" encoding="UTF-8"?>\n' +
-'<!DOCTYPE book PUBLIC "-//OASIS//DTD DocBook XML V4.5//EN" "http://www.oasis-open.org/docbook/xml/4.5/docbookx.dtd">\n' +
-'<?asciidoc-toc?>\n' +
-'<?asciidoc-numbered?>\n' +
-'<book lang="en">\n' +
-'<bookinfo>\n' +
-'<title>DocTitle</title>\n' +
-'<date>2014-01-01</date>\n' +
-'</bookinfo>\n' +
-'<chapter id="_test">\n' +
-'<title>Test</title>\n' +
-'\n' +
-'</chapter>\n' +
-'</book>');
+
+    assert.throws(() => {
+      asciidoctor.convert(':doctitle: DocTitle\n:docdate: 2014-01-01\n== Test', options);
+    });
   });
 
   it('should produce a docbook5 document when backend=docbook5', function () {
