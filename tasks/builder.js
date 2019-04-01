949ca86433259b849be197c281bc24135ebe38ed
Guillaume Grossetie <ggrossetie@gmail.com>
2019-04-01 08:41:06 +0200

    Use modern JavaScript
    - Use class
    - Use chai/expect
    - Use async/await
    - Add a linter (standard)

---
+++ tasks/builder.js
@@ -0,0 +1,109 @@
+'use strict'
+const fs = require('fs')
+const log = require('bestikk-log')
+const bfs = require('bestikk-fs')
+const Download = require('bestikk-download')
+const OpalBuilder = require('opal-compiler').Builder
+
+const concat = (message, files, destination) => {
+  log.debug(message)
+  bfs.concatSync(files, destination)
+}
+
+const templateFile = function (templateFile, context, outputFile) {
+  const template = fs.readFileSync(templateFile, 'utf8')
+  const lines = template.split('\n')
+  lines.forEach(function (line, index, result) {
+    if (line in context) {
+      result[index] = context[line]
+    }
+  })
+  const content = lines.join('\n')
+  fs.writeFileSync(outputFile, content, 'utf8')
+}
+
+const copyToDist = () => {
+  log.task('copy to dist/')
+  removeDistDirSync()
+  bfs.copySync('build/asciidoctor-docbook.js', 'dist/main.js')
+}
+
+const removeDistDirSync = () => {
+  log.debug('remove dist directory')
+  bfs.removeSync('dist')
+  bfs.mkdirsSync('dist')
+}
+
+const removeBuildDirSync = () => {
+  log.debug('remove build directory')
+  bfs.removeSync('build')
+  bfs.mkdirsSync('build')
+}
+
+const generateUMD = () => {
+  log.task('generate UMD')
+  const docbookFiles = [
+    'build/asciidoctor-docbook5.js'
+  ]
+  concat('Asciidoctor DocBook 5', docbookFiles, 'build/asciidoctor-docbook.js')
+  const asciidoctorDocbookTemplateContext = {
+    '//#{asciidoctorDocbookCode}': fs.readFileSync('build/asciidoctor-docbook.js', 'utf8')
+  }
+  templateFile('src/template-asciidoctor-docbook.js', asciidoctorDocbookTemplateContext, 'build/asciidoctor-docbook.js')
+}
+
+const clean = () => {
+  log.task('clean')
+  removeBuildDirSync()
+}
+
+const compile = () => {
+  log.task('compile')
+  const module = 'docbook5'
+  const opalBuilder = OpalBuilder.create()
+  opalBuilder.appendPaths('build/asciidoctor/lib')
+  opalBuilder.appendPaths('node_modules/opal-compiler/src/stdlib')
+  opalBuilder.appendPaths('lib')
+  opalBuilder.setCompilerOptions({ dynamic_require_severity: 'ignore' })
+  fs.writeFileSync(`build/asciidoctor-${module}.js`, opalBuilder.build(`asciidoctor/converter/${module}`).toString(), 'utf8')
+}
+
+class Builder {
+  constructor () {
+    this.asciidoctorCoreVersion = '2.0.4'
+    this.download = new Download({})
+  }
+
+  async build () {
+    if (process.env.SKIP_BUILD) {
+      log.info('SKIP_BUILD environment variable is true, skipping "build" task')
+      return
+    }
+    if (process.env.DRY_RUN) {
+      log.debug('build')
+      return
+    }
+    const start = process.hrtime()
+
+    try {
+      clean()
+      await this.downloadDependencies()
+      await compile()
+      generateUMD()
+      copyToDist()
+    } catch (e) {
+      console.log(e)
+      process.exit(1)
+    }
+
+    log.success('Done in ' + process.hrtime(start)[0] + 's')
+  }
+
+  async downloadDependencies () {
+    log.task('download dependencies')
+    await this.download.getContentFromURL(`https://codeload.github.com/asciidoctor/asciidoctor/tar.gz/v${this.asciidoctorCoreVersion}`, 'build/asciidoctor.tar.gz')
+    await bfs.untar('build/asciidoctor.tar.gz', 'asciidoctor', 'build')
+  }
+}
+
+module.exports = Builder
--- test/spec.js
+++ test/spec.js
@@ -1,32 +1,29 @@
-var asciidoctor = require('@asciidoctor/core')();
-require('../dist/main.js')(); // Asciidoctor DocBook
-var assert = require('assert');
+const asciidoctor = require('@asciidoctor/core')()
+require('../dist/main.js')() // Asciidoctor DocBook
+const chai = require('chai')
+const expect = chai.expect
 
-describe('Rendering', function () {
+describe('Rendering', () => {
+  it('should not produce a docbook45 document when backend=docbook45', () => {
+    const options = { attributes: ['backend=docbook45', 'doctype=book'], header_footer: true }
+    expect(() => asciidoctor.convert(':doctitle: DocTitle\n:docdate: 2014-01-01\n== Test', options)).to.throw()
+  })
 
-  it('should not produce a docbook45 document when backend=docbook45', function () {
-    var options = {attributes: ['backend=docbook45', 'doctype=book'], header_footer: true};
+  it('should produce a docbook5 document when backend=docbook5', () => {
+    const options = { attributes: { backend: 'docbook5', doctype: 'book' }, standalone: true }
+    const html = asciidoctor.convert(':doctitle: DocTitle\n:docdate: 2014-01-01\n== Test', options)
+    expect(html).to.equal(`<?xml version="1.0" encoding="UTF-8"?>
+<?asciidoc-toc?>
+<?asciidoc-numbered?>
+<book xmlns="http://docbook.org/ns/docbook" xmlns:xl="http://www.w3.org/1999/xlink" version="5.0" xml:lang="en">
+<info>
+<title>DocTitle</title>
+<date>2014-01-01</date>
+</info>
+<chapter xml:id="_test">
+<title>Test</title>
 
-    assert.throws(() => {
-      asciidoctor.convert(':doctitle: DocTitle\n:docdate: 2014-01-01\n== Test', options);
-    });
-  });
-
-  it('should produce a docbook5 document when backend=docbook5', function () {
-    var options = {attributes: ['backend=docbook5', 'doctype=book'], header_footer: true};
-    var html = asciidoctor.convert(':doctitle: DocTitle\n:docdate: 2014-01-01\n== Test', options);
-    assert.equal(html, '<?xml version="1.0" encoding="UTF-8"?>\n' +
-'<?asciidoc-toc?>\n' +
-'<?asciidoc-numbered?>\n' +
-'<book xmlns="http://docbook.org/ns/docbook" xmlns:xl="http://www.w3.org/1999/xlink" version="5.0" xml:lang="en">\n' +
-'<info>\n' +
-'<title>DocTitle</title>\n' +
-'<date>2014-01-01</date>\n' +
-'</info>\n' +
-'<chapter xml:id="_test">\n' +
-'<title>Test</title>\n' +
-'\n' +
-'</chapter>\n' +
-'</book>');
-  });
-});
+</chapter>
+</book>`)
+  })
+})
