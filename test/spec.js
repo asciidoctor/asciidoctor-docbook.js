eebf95d5f5da6901fd54e680c4c5db5f69984c3e
Guillaume Grossetie <ggrossetie@gmail.com>
2020-04-13 11:23:36 +0200

    resolves #16 add a test case on docinfo

--- test/spec.js
+++ test/spec.js
@@ -27,3 +27,38 @@ describe('Rendering', () => {
 </book>`)
   })
 })
+
+describe('Docinfo', () => {
+  it('should include a private docinfo header', () => {
+    const options = { backend: 'docbook', standalone: true, safe: 'safe', to_file: false }
+    const xml = asciidoctor.convertFile(`${__dirname}/fixtures/doc.adoc`, options)
+    expect(xml).to.equal(`<?xml version="1.0" encoding="UTF-8"?>
+<?asciidoc-toc?>
+<?asciidoc-numbered?>
+<book xmlns="http://docbook.org/ns/docbook" xmlns:xl="http://www.w3.org/1999/xlink" version="5.0" xml:lang="en">
+<info>
+<title>Document Title</title>
+<date>2020-04-13</date>
+<author>
+<personname>
+<firstname>Author</firstname>
+<surname>Name</surname>
+</personname>
+</author>
+<authorinitials>AN</authorinitials>
+<revhistory>
+<revision>
+<revnumber>1.0</revnumber>
+<date>2020-04-13</date>
+<authorinitials>AN</authorinitials>
+</revision>
+</revhistory>
+<edition>1.0</edition>
+</info>
+<preface>
+<title></title>
+<simpara>This is a sample document.</simpara>
+</preface>
+</book>`)
+  })
+})
