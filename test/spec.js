9180519c6ff6da16879da4f29eee68229ed01d92
Guillaume Grossetie <ggrossetie@gmail.com>
2020-06-23 17:37:03 +0200

    👷 Replace Travis by GitHub Actions (#27)

--- test/spec.js
+++ test/spec.js
@@ -6,12 +6,16 @@ const expect = chai.expect
 describe('Rendering', () => {
   it('should not produce a docbook45 document when backend=docbook45', () => {
     const options = { attributes: ['backend=docbook45', 'doctype=book'], header_footer: true }
-    expect(() => asciidoctor.convert(':doctitle: DocTitle\n:docdate: 2014-01-01\n== Test', options)).to.throw()
+    expect(() => asciidoctor.convert(`:doctitle: DocTitle
+:docdate: 2014-01-01
+== Test`, options)).to.throw()
   })
 
   it('should produce a docbook5 document when backend=docbook5', () => {
     const options = { attributes: { backend: 'docbook5', doctype: 'book' }, standalone: true }
-    const html = asciidoctor.convert(':doctitle: DocTitle\n:docdate: 2014-01-01\n== Test', options)
+    const html = asciidoctor.convert(`:doctitle: DocTitle
+:docdate: 2014-01-01
+== Test`, options)
     expect(html).to.equal(`<?xml version="1.0" encoding="UTF-8"?>
 <?asciidoc-toc?>
 <?asciidoc-numbered?>
